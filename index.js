3a348f2b77990a1d8689b97e8c7b07eaa605fa11
Ursine Raven <UrsineRaven@gmail.com>
2019-10-17 09:25:10 -0500

    make proxy use subdomains and cleanup

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 var https = require('https');
 var httpProxy = require('http-proxy');
 var express = require('express');
-var HttpProxyRules = require('http-proxy-rules'); // TODO: just do this manually instead of using this package (since it doesn't support matching sub-domains). Maybe look for different package that does support sub-domain matching (subdomain-router is close, but doesn't support different hosts and maybe not modifying response headers)
+var jsyaml = require('js-yaml');
+var fs = require('fs');
 
-var proxyRules = new HttpProxyRules({
-    rules: {
-        '.*/minecraft.*': 'https://localhost:3052',
-        '.*/lexie.*': 'https://localhost:3050',
-        '.*/react-practice.*': 'https://localhost:3055'
-    },
-    default: 'https://localhost:8080'
-});
+var config = jsyaml.safeLoad(fs.readFileSync(__dirname + '/config.yml', 'utf8'));
+
 var proxy = httpProxy.createProxy();
 
-var bodyParser = require('body-parser');
 var app = express();
 app.set('trust proxy', true);
+app.set('subdomain offset', (config && config.subdomainOffset) || 2);
 app.use(function(req,res,next){
     try{
-        var target = proxyRules.match(req);
-        if (target) {
-            console.log("TARGET", target, req.url);
+        console.dir(req.subdomains);
+        var subdomains = req.subdomains.slice();
+        var target = config && config.proxyRules;
+        while (subdomains.length > 0 && typeof target !== 'string') {
+            var sd = subdomains.shift();
+            target = target && target[sd];
+        }
+        console.dir(target);
+        if (typeof target === 'object') {
+            target = target[null];
+        }
+        console.dir(target);
+        if (typeof target === 'string') {
+            //console.log("TARGET", target, req.url);
             return proxy.web(req, res, {
                 target: target
             }, function(e) {
-                console.log('PROXY ERR', e, req.ip, '=>', req.hostname, req.originalUrl);
+                //console.log('PROXY ERR', e, req.ip, '=>', req.hostname, req.originalUrl);
             });
         } else {
             res.sendStatus(404);
@@ -33,31 +39,17 @@ app.use(function(req,res,next){
         res.sendStatus(500);
     }
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-//===============PORT=================
+//===============PORTS=================
 var httpPort = 80;
-var httpsPort = process.env.PORT || 443;
-//===============SSL=================
+var httpsPort = 443;
+//===============HTTP=================
 var http_server = require('http').createServer();
 http_server.on('request', require('redirect-https')({ port: httpsPort }));
-http_server.listen(80, function () { console.log(`Listening on port ${httpPort} for http`); });
-
-var fs = require('fs'),
-  https = require('https'),
+http_server.listen(80, function () { console.log(`Listening on port ${httpPort} for redirect to HTTPS.`); });
+//===============HTTPS=================
+var https = require('https'),
   key = fs.readFileSync(__dirname + '/ssl/key.pem'),
   cert = fs.readFileSync(__dirname + '/ssl/cert.pem');
-var mainserver = https.createServer({ key: key, cert: cert }, app).listen(httpsPort);
-console.log('Listening on ' + httpsPort + '!');
-//mainserver.on('listening', onListening);
-mainserver.on('error', function (error, req, res) {
-    var json;
-    console.log('proxy error', error);
-    if (!res.headersSent) {
-        res.writeHead(500, { 'content-type': 'application/json' });
-    }
+var mainserver = https.createServer({ key: key, cert: cert }, app).listen(httpsPort, function () { console.log('Listening on ' + httpsPort + '!'); });
 
-    json = { error: 'proxy_error', reason: error.message };
-    res.end(JSON.stringify(json));
-});
